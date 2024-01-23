09e4df1f80ff6010491af1d5df8ff1aab28cec7c
bato0906 <bato0906@gmail.com>
2024-01-23 11:49:47 +0900

    make scroll animation when a tag clicked

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 
-$("header a").click(function(e){
+$("a").click(function(e){
     const href = $(this).attr("href"); //cf)prop()은 제어할 때 쓴다.
     const offTop = $(href).offset().top; //문서의 탑에서부터 오프셋
     $("html, body").stop().animate({scrollTop: offTop}, 500);
